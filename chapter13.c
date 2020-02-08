2675e51d6dd46550b54795e2c5cd4cc9eae3d4c5
rubblesky <2018211896@mail.hfut.edu.cn>
2020-02-08 21:00:01 +0800

    add chapter13

---
+++ chapter13.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    //指针数组
+    char *ptr[] = {
+        "hello",
+        "how are you",
+        "nice to meet you"
+    } ;
+
+    //二维数组
+    char c[][50] = {
+        "hello",
+        "how are you",
+        "nice to meet you"
+    };
+}
