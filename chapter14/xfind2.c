526a738b9475d102043763ced9fb888a11d31914
rubblesky <2018211896@mail.hfut.edu.cn>
2020-02-11 09:16:31 +0800

    revise chapter14

--- chapter14/xfind2.c
+++ chapter14/xfind2.c
@@ -5,6 +5,7 @@
 //返回字符串长度
 int getLine(char s[], int lim){
     int c,i;
+    int static num = 0;
     for(i = 0;i<lim-1&&(c=getchar())!=EOF&&c!='\n';++i)
         s[i] = c;
     if(c == '\n'){
@@ -19,9 +20,12 @@ int main(int argc,char *argv[]){
     char line[MAXLINE];
     long lineno = 0;
     int c, except =0,number =0, found = 0;
-
+    //printf("*argv[2] = %s\n",argv[2]);
+    printf("argc = %d\n",argc);
     while(--argc > 0 && (*++argv)[0] == '-'){
+        //printf("argv[0] = %s\n",*argv);
         while (c = *++argv[0]){
+            //printf("c = %c\n",**argv);
             switch (c)
             {
             case 'x':
@@ -37,19 +41,21 @@ int main(int argc,char *argv[]){
                 break;
             }
         }
-        
-        if(argc != 1)
-            printf("Usage: find -x -n pattern\n");
-        else
-            while (getLine(line,MAXLINE)>0){
-                lineno++;
-                if((strstr(line,*argv) != NULL)!=except){
-                    if(number)
-                        printf("%ld:",lineno);
-                    printf("%s",line);
-                    found++;
-                }
-            }      
     }
+    printf("argc2 = %d\n",argc);
+    //printf("*argv = %s\n",*(argv-1));
+    if(argc != 1)
+        printf("Usage: find -x -n pattern\n");
+    else
+        while (getLine(line,MAXLINE)>0){
+            lineno++;
+            //printf("*argv = %s\n",*argv);
+            if((strstr(line,*argv) != NULL)!=except){
+                if(number)
+                    printf("%ld:",lineno);
+                printf("%s",line);
+                found++;
+            }
+        }      
     return found;
 }
